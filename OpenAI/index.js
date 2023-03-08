f17408c46d71b0bf2a7290ab4162c3295987e9f1
sonofdust <sonofdust@gmail.com>
2023-03-08 11:52:00 -0700

    Separte models.

--- OpenAI/index.js
+++ OpenAI/index.js
@@ -3,19 +3,23 @@ require("dotenv").config();
 
 const configuration = new Configuration({apiKey: process.env.OPEN_AI_KEY});
 const openai = new OpenAIApi(configuration);
+const text_davinci_003 = {
+  model: "text-davinci-003",
+  prompt: "",
+  max_tokens: 64,
+  temperature: 0,
+  top_p: 1.0,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+  stop: ["/n"],
+};
 
 const getResponse = async (req, res) => {
   try {
     const {prompt} = req.body;
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-      max_tokens: 64,
-      temperature: 0,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-      stop: ["/n"],
+      ...text_davinci_003,
+      prompt,
     });
     return res
       .status(200)
