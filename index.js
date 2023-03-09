36eb933ffc74e6b0ded6a1b28476de980e80712b
sonofdust <sonofdust@gmail.com>
2023-03-09 06:50:35 -0700

    Change folder structure.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getResponse} = require("./OpenAI");
+const {getResponse} = require("./Models/text_davinci_003");
 const app = express();
 app.use(express.json());
 app.post("/chat", async (req, res) => getResponse(req, res));
